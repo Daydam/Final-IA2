c454e968e887ac501bfca8befde734191d787242
Daydam <daydam_martin@users.noreply.github.com>
2019-02-23 00:36:43 -0300

    Initial commit

---
+++ Assets/Script/GOAP/GOAPAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOAPAction : MonoBehaviour
+{
+    Func<GOAPState, bool> preconditions;
+    Func<GOAPState> effects;
+    string actionName;
+    public string Name { get { return actionName; } }
+    float cost;
+    public float Cost { get { return cost; } }
+
+    public GOAPAction (string name)
+    {
+        this.actionName = name;
+        cost = 1f;
+        preconditions = a => true;
+        effects = () => new GOAPState();
+    }
+
+    public GOAPAction SetCost (float cost)
+    {
+        this.cost = cost;
+        return this;
+    }
+
+    public GOAPAction SetPreconditions(Func<GOAPState, bool> preconditions)
+    {
+        this.preconditions = preconditions;
+        return this;
+    }
+
+    public GOAPAction SetEffects(Func<GOAPState> effects)
+    {
+        this.effects = effects;
+        return this;
+    }
+}
---
+++ Assets/Script/GOAP/GOAPState.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GOAPState
+{
+    /*TBH, I didn't want to use object, but found no better choice for using generic data as a value*/
+
+    /*I could've done a class-saving structure as seen in Memento, with a generic Memory class for storing different scripts's values,
+     * but I felt like this would be better. This way, I can get a really customizable value list, like for example, the distance from an enemy to my character,
+     * without having to store the enemy position, my character position, and lots of other stuff that I may not care about.
+     * I can store literally anything I want, and I fucking love it.*/
+    
+    Dictionary<string, object> values;
+    public Dictionary<string, object> Values { get { return values; } set { values = value; } }
+
+    /*This will come in handy for some constantly changing states, such as positions and stuff. It turns the GOAPState into something more like a memento,
+     * capable of REALLY keeping track of the world's state, managing details like time, distances, and lots of other stuff that would otherwise become ENGORROSO AS FUCK.*/
+    Dictionary<string, Func<object>> updates;
+    public Dictionary<string, Func<object>> Updates { get { return updates; } set { updates = value; } }
+    public GOAPAction generatingAction = null;
+    public int stepId = 0;
+
+    public bool Equals(GOAPState obj)
+    {
+        var result =
+            obj != null
+            && obj.generatingAction == generatingAction
+            && obj.values.Count == values.Count
+            && obj.values.All(kv => values.ContainsValue(kv));
+        return result;
+    }
+
+    public void UpdateValues()
+    {
+        foreach (string key in updates.Keys)
+        {
+            values[key] = updates[key]();
+        }
+    }
+}
